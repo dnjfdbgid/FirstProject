976d3ea09d784193984a7e441d04fb1cdb2f8085
dnjfdbgid <dnjfdbgid@gmail.com>
2014-04-21 11:11:38 +0900

    관리자 기능 추가

---
+++ FirstApp/src/admin/Manager.java
@@ -0,0 +1,8 @@
+package admin;
+public class Manager {
+	public static void main(String[] args) {
+		System.out.println("첫번째 프로젝트");
+		System.out.println("Admin에서 수정");
+		System.out.println("Manager.java 생성");
+	}
+}
